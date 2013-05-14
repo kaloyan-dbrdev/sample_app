08d13efa76e15439ad73c4ad6bcb6d53194ff576
Kaloyan Yordanov <kaloyan.yordanov@dbrdev.com>
2013-05-14 17:47:27 +0300

    Added Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -58,4 +58,23 @@ describe "Static pages" do
       page.should have_content('About Us')
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+  end
 end
